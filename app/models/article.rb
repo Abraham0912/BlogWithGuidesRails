d81b814924774e76d10553c49d7fa46427114f0d
Abraham0912 <09120912ahn@gmail.com>
2021-03-17 20:43:58 -0600

    Terminado el paso 8 de la documentacion de rails eliminacion de articulos y comentarios

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   
-  has_many :comments #Relacion uno a muchos con la tabla comments = model Comment
+  has_many :comments, dependent: :destroy #Relacion uno a muchos con la tabla comments = model Comment, dependent: :destroy
 
   validates :title, presence: true,
                     length: { minimum: 5 } # todos los artículos tengan un título de al menos cinco caracteres
