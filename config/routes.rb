43fd2b2f494b3c646f893d7ba2e356ca5ae73a68
Abraham0912 <09120912ahn@gmail.com>
2021-03-17 19:49:05 -0600

    generate model Comment with references at article

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#index'
 
-  get 'welcome/testing'
+  #get 'welcome/testing'
+  get 'hola', to: 'welcome#testing'
 
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
